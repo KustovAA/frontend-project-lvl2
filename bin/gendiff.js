f144c5040f6502153d2bfb1450a11b48e25d07af
andrey-kustov <andrey-kustov@yandex-team.ru>
2021-01-02 16:04:21 +0300

    настраивает проект, добавляет базовый вывод информации об утилите gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+
+const program = new commander.Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .parse(process.argv);
