23fd2f59c99fdac281635f90285d4cbe10672fa3
andrey-kustov <andrey-kustov@yandex-team.ru>
2021-01-03 03:07:05 +0300

    добавляет сравнение плоских файлов для формата yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,10 +3,18 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
-test('compare plain objects', async () => {
+test('compare plain objects json', async () => {
   const expected = await fs.readFile(path.resolve(process.cwd(), '__fixtures__/plain-objects.fixture.txt'), 'utf-8');
   const filepath1 = '__fixtures__/file1.json';
   const filepath2 = '__fixtures__/file2.json';
+  const actual = await genDiff(filepath1, filepath2);
+  expect(actual).toBe(expected);
+});
+
+test('compare plain objects yaml', async () => {
+  const expected = await fs.readFile(path.resolve(process.cwd(), '__fixtures__/plain-objects.fixture.txt'), 'utf-8');
+  const filepath1 = '__fixtures__/file1.yml';
+  const filepath2 = '__fixtures__/file2.yml';
   const actual = await genDiff(filepath1, filepath2);
   expect(actual).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import _ from 'lodash';
+import yaml from 'js-yaml';
 
-const genDiff = async (filepath1, filepath2) => {
+const parserMap = {
+  json: (file) => JSON.parse(file),
+  yaml: (file) => yaml.load(file),
+};
+
+const extMap = {
+  '.json': 'json',
+  '.yml': 'yaml',
+  '.yaml': 'yaml',
+};
+
+const parse = (file, ext) => parserMap?.[ext]?.(file);
+
+const readFiles = async (filepath1, filepath2) => {
   const path1 = path.resolve(process.cwd(), filepath1);
   const path2 = path.resolve(process.cwd(), filepath2);
+  const ext = extMap[path.extname(filepath1)];
   const files = await Promise.all([fs.readFile(path1, 'utf-8'), fs.readFile(path2, 'utf-8')]);
-  const [file1, file2] = files.map((file) => JSON.parse(file));
+  const [file1, file2] = files.map((file) => parse(file, ext));
+  return { file1, file2 };
+};
+
+const genDiff = async (filepath1, filepath2, format) => {
+  const { file1, file2 } = await readFiles(filepath1, filepath2, format);
   const result = ['', '{'];
 
   _.forOwn(file2, (value, key) => {
