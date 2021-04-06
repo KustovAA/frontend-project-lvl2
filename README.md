4beb5dfa80ee487c9a746b6fc367d2a2e611bb39
andrey-kustov <andrey-kustov@yandex-team.ru>
2021-04-06 21:01:58 +0300

    Исправляет тесты, добавляет ридми и аскинему

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-### Hexlet tests and linter status:
+#Вычислитель отличий
+
 [![Actions Status](https://github.com/KustovAA/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/KustovAA/frontend-project-lvl2/actions)
 [![Actions Status](https://github.com/KustovAA/frontend-project-lvl2/workflows/ci-check/badge.svg)](https://github.com/KustovAA/frontend-project-lvl2/actions)
 <a href="https://codeclimate.com/github/KustovAA/frontend-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/a18f0499b04f7a767985/maintainability" /></a>
 <a href="https://codeclimate.com/github/KustovAA/frontend-project-lvl2/test_coverage"><img src="https://api.codeclimate.com/v1/badges/a18f0499b04f7a767985/test_coverage" /></a>
+
+## Установка и запуск
+
+* `make install` установка утилиты
+* `gendiff -h` запуск утилиты (вызов справки)
+
+## Дополнительно
+
+* `make lint` проверка линтером
+* `make test` проверка тестами
+
+## Демонстрация
+
+[![asciicast](https://asciinema.org/a/tQzbd9LfR9t9RwEZhcrlpJB9c.svg)](https://asciinema.org/a/tQzbd9LfR9t9RwEZhcrlpJB9c)
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,18 +3,26 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../index.js';
 
+const getFixture = (filename) => fs.readFileSync(path.resolve(process.cwd(), path.join('__fixtures__', filename)), 'utf-8').trim();
+
+const fixtures = {
+  plain: getFixture('result-plain.txt'),
+  stylish: getFixture('result-stylish.txt'),
+  json: getFixture('result-json.txt'),
+};
+
 describe.each(['json', 'yml'])('compare objects %s', (ext) => {
   const filepath1 = path.join('__fixtures__', `file1.${ext}`);
   const filepath2 = path.join('__fixtures__', `file2.${ext}`);
 
-  test.each(['plain', 'stylish', 'json'])('%s', async (format) => {
-    const expected = fs.readFileSync(path.resolve(process.cwd(), path.join('__fixtures__', `result-${format}.txt`)), 'utf-8').trim();
-    const actual = genDiff(filepath1, filepath2, format);
+  test.each(['plain', 'stylish', 'json'])('%s', async (formatter) => {
+    const expected = fixtures[formatter];
+    const actual = genDiff(filepath1, filepath2, formatter);
     expect(actual).toBe(expected);
   });
 
   test('no formatter', async () => {
-    const expected = fs.readFileSync(path.resolve(process.cwd(), path.join('__fixtures__', 'result-stylish.txt')), 'utf-8').trim();
+    const expected = fixtures.stylish;
     const actual = genDiff(filepath1, filepath2);
     expect(actual).toBe(expected);
   });
