a42d7baa9456cdb9570589b381a60d353106e618
andrey-kustov <andrey-kustov@yandex-team.ru>
2021-04-09 13:32:36 +0300

    Изменяет нейминг в форматтерах, немного рефакторит код

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const transformValue = (value) => {
+const stringify = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
@@ -9,19 +9,19 @@ const transformValue = (value) => {
     return `'${value}'`;
   }
 
-  return value;
+  return `${value}`;
 };
 
 const plainMapping = {
   unchanged: () => [],
-  changed: ({ key, value1, value2 }, path = []) => `Property '${path.concat(key).join('.')}' was updated. From ${transformValue(value2)} to ${transformValue(value1)}`,
-  added: ({ key, value }, path = []) => `Property '${path.concat(key).join('.')}' was added with value: ${transformValue(value)}`,
+  changed: ({ key, value1, value2 }, path = []) => `Property '${path.concat(key).join('.')}' was updated. From ${stringify(value2)} to ${stringify(value1)}`,
+  added: ({ key, value }, path = []) => `Property '${path.concat(key).join('.')}' was added with value: ${stringify(value)}`,
   removed: ({ key }, path = []) => `Property '${path.concat(key).join('.')}' was removed`,
   recursive: (
     { key, keys }, path = [],
   ) => keys.map((node) => plainMapping[node.type](node, path.concat(key))),
 };
 
-const plain = (actions) => actions.map((action) => plainMapping[action.type](action)).flat(Infinity).join('\n');
+const plain = (nodes) => _.flatMapDeep(nodes, (node) => plainMapping[node.type](node)).join('\n');
 
 export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,50 +2,38 @@ import _ from 'lodash';
 
 const indent = 2;
 
-const genIndent = (depth) => ' '.repeat(depth);
+const generateIndent = (depth) => ' '.repeat(depth);
 
-const transformValue = (value, depth = 0, mapping) => {
+const stringify = (value, depth = 0, mapping) => {
   if (_.isPlainObject(value)) {
     return ['{']
       .concat(
         _.keys(value)
           .map((key) => mapping.unchanged({ key, value: value[key] }, depth + indent * 2)),
       )
-      .concat([`${genIndent(depth)}  }`])
+      .concat([`${generateIndent(depth)}  }`])
       .join('\n');
   }
 
-  return value;
+  return `${value}`;
 };
-
-const signMap = {
-  added: '+',
-  removed: '-',
-  unchanged: ' ',
-};
-
-const genRow = ({ key, value }, sign, depth, mapping) => `${genIndent(depth)}${signMap[sign]} ${key}: ${transformValue(value, depth, mapping)}`;
-
-const genAddedRow = ({ key, value }, depth, mapping) => genRow({ key, value }, 'added', depth, mapping);
-
-const genRemovedRow = ({ key, value }, depth, mapping) => genRow({ key, value }, 'removed', depth, mapping);
-
-const genUnchangedRow = ({ key, value }, depth, mapping) => genRow({ key, value }, 'unchanged', depth, mapping);
-
-const genChangedRow = ({ key, value1, value2 }, depth, mapping) => `${genRemovedRow({ key, value: value2 }, depth, mapping)}\n${genAddedRow({ key, value: value1 }, depth, mapping)}`;
 
 const mapping = {
-  unchanged: ({ key, value }, depth = indent) => genUnchangedRow({ key, value }, depth, mapping),
-  changed: (value, depth = indent) => genChangedRow(value, depth, mapping),
-  added: ({ key, value }, depth = indent) => genAddedRow({ key, value }, depth, mapping),
-  removed: ({ key, value }, depth = indent) => genRemovedRow({ key, value }, depth, mapping),
-  recursive: ({ key, keys }, depth = indent) => [`${genIndent(depth)}  ${key}: {`].concat(keys.map((node) => mapping[node.type](node, depth + indent * indent))).concat([`${genIndent(depth)}  }`]).join('\n'),
+  added: ({ key, value }, depth = indent) => `${generateIndent(depth)}+ ${key}: ${stringify(value, depth, mapping)}`,
+  removed: ({ key, value }, depth = indent) => `${generateIndent(depth)}- ${key}: ${stringify(value, depth, mapping)}`,
+  unchanged: ({ key, value }, depth = indent) => `${generateIndent(depth)}  ${key}: ${stringify(value, depth, mapping)}`,
+  changed: ({ key, value1, value2 }, depth = indent) => `${mapping.removed({ key, value: value2 }, depth)}\n${mapping.added({ key, value: value1 }, depth)}`,
+  recursive: ({ key, keys }, depth = indent) => [`${generateIndent(depth)}  ${key}: {`].concat(keys.map((node) => mapping[node.type](node, depth + indent * indent))).concat([`${generateIndent(depth)}  }`]).join('\n'),
 };
 
-const stylish = (actions) => ['{'].concat(actions.map(({
-  type, key, value, value2, value1, keys,
-}) => mapping[type]({
-  key, value, value2, value1, keys, type,
-}))).concat(['}']).flat(Infinity).join('\n');
+const startNode = ['{'];
+const finishNode = ['}'];
+const mapNode = (node) => mapping[node.type](node);
+
+const stylish = (nodes) => startNode
+  .concat(nodes.map(mapNode))
+  .concat(finishNode)
+  .flat(Infinity)
+  .join('\n');
 
 export default stylish;
